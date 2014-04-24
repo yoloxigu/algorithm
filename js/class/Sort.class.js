da3d5750ba45845e93d753ca619923b8b94178e7
yoloxigu <yoloxigu@gmail.com>
2014-04-24 20:04:55 +0800

    Rejected commit: Default

--- js/class/Sort.class.js
+++ js/class/Sort.class.js
@@ -1,37 +1,125 @@
-function Sort() {}
+function Sort() {
+    this.arr = [];
+    this.paramChecked = false;
+    this.haveParam = true;
+    this.isArray = false;
+    this.isString = false;
+    this.isNumeric = false;
+    this.orderby = 'asc';
+    this.message = '';
+}
 
-Sort.prototype.quickSort = function(arr) {
-    if (arr.length <= 1) {
-        return arr;
-    }
+Sort.prototype = {
+    checkParam: function(arguments) {
+        this.paramChecked = true;
+        if(arguments.length == 0) {
+            this.haveParam = false;
+            return this.message = 'Have not a param at invoked!';
+        }
 
-    var orderby = arguments[1];
-    switch(orderby) {
-        case undefined:
-            orderby = 'aesc';
-            break;
-        case 'aesc':
-            break;
-        case 'desc':
-            break;
-        default:
-            orderby = 'aesc';
-    }
+        if(arguments[0] instanceof Array) {
+            this.isArray = true;
+        }
 
-    if (arr.length > 1) {
-        var left = [],
-            right = [],
-            middle = arr[0],
-            len = arr.length;
+        if(typeof arguments[0] == 'string') {
+            this.isString = true;
+        }
 
-        for(var i = 1; i < len; i++) {
-            if (orderby == 'aesc' ? arr[i] <= middle : arr[i] >= middle) {
-                left.push(arr[i]);
+        if(typeof arguments[0] == 'number') {
+            this.isNumeric = true;
+        }
+
+        switch(arguments[1]) {
+            case undefined:
+                return this.orderby = 'asc';
+            case 'asc':
+                return this.orderby = 'asc';
+            case 'desc':
+                return this.orderby = 'desc';
+            default:
+                return this.orderby = 'asc';
+        }
+    },
+    popSort: function(value, orderby) {
+        if(!this.paramChecked) {
+            this.checkParam(arguments);
+        }
+
+        if(!this.haveParam) {
+            return this.message;
+        }
+
+        if(this.isNumeric) {
+            if(value < 0) {
+                value = value.toString().substr(1).split('');
+                this.isNumeric = false;
             } else {
-                right.push(arr[i]);
+                value = value.toString().split('');
+                this.isNumeric = false;
             }
         }
 
-        return this.quickSort(left, orderby).concat(middle, this.quickSort(right, orderby));
+        if(this.isString) {
+            value = value.split('');
+            this.isString = false;
+        }
+
+        if(value.length <= 1) {
+            return value;
+        }
+
+        for(var i = 0, len = value.length; i < len; i++) {
+            for(var j = len - 1; j > i; j--) {
+                if(this.orderby == 'asc' ? value[j] < value[j - 1] : value[j] > value[j - 1]) {
+                   var temp = value[j];
+                       value[j] = value[j - 1];
+                       value[j - 1] = temp;
+                }
+            }
+        }
+        return value;
+    },
+    quickSort: function(value, orderby) {
+        if(!this.paramChecked) {
+            this.checkParam(arguments);
+        }
+
+        if(!this.haveParam) {
+            return this.message;
+        }
+
+        if(this.isNumeric) {
+            if(value < 0) {
+                value = value.toString().substr(1).split('');
+                this.isNumeric = false;
+            } else {
+                value = value.toString().split('');
+                this.isNumeric = false;
+            }
+        }
+
+        if(this.isString) {
+            value = value.split('');
+            this.isString = false;
+        }
+
+        if(value.length <= 1) {
+            return value;
+        }
+
+        var left = [],
+            right = [],
+            middle = value[0],
+            len = value.length;
+
+        for(var i = 1; i < len; i++) {
+            if (this.orderby == 'asc' ? value[i] <= middle : value[i] >= middle) {
+                left.push(value[i]);
+            } else {
+                right.push(value[i]);
+            }
+        }
+        return this.quickSort(left, this.orderby).concat(middle, this.quickSort(right, this.orderby));
     }
 };
+
