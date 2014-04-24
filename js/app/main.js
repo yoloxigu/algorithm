32124b52bd870171c492254f2dab04605dd36af5
yoloxigu <yoloxigu@gmail.com>
2014-04-24 14:35:09 +0800

    Rejected commit: Default

--- js/app/main.js
+++ js/app/main.js
@@ -1,9 +1,8 @@
-define(["jquery", "Sort"], function($) {
+define(["jquery", "Sort"], function() {
     var arr = [11, 77, 23, 26, 97, 77, 45, 27, 34, 2, 1, 3];
     //var arr = ['ac', 'ba', 'ab', 'g', 'h', 'z', 'x', 'y', 'n', 'm'];
     var sort = new Sort();
-    var sorted = sort.quickSort(arr);
+    var sorted = sort.quickSort(arr, 'desc');
     var str = sorted.join(' ');
     console.log(str);
-    console.log(sort.counter);
 });
