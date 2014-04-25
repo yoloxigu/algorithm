7d0719c81656fbb43ca5359ec361a98263718141
yoloxigu <yoloxigu@gmail.com>
2014-04-25 12:19:02 +0800

    Rejected commit: Default

--- class/Sort.class.php
+++ class/Sort.class.php
@@ -1,39 +1,177 @@
 <?php
+/**
+ * This class for array sort
+ * @author yoloxigu
+ */
 class Sort {
-    public function quickSort($arr, $orderby = 'aesc') {
-        if(sizeof($arr) > 1) {
-            $k = $arr[0];
-            $x = array();
-            $y = array();
-            $_size = count($arr);
+    private $valueType;
+    private $paramChecked = false;
+    private $orderby = 'asc';
 
-            switch($orderby) {
-                case Null:
-                    $orderby = 'aesc';
-                    break;
-                case 'aesc':
-                    break;
-                case 'desc':
-                    break;
-                default:
-                    $orderby = 'aesc';
-                    break;
+    /**
+     * @param mixed $value
+     * @param string $orderby
+     * @return void
+     */
+    private function checkParam($value, $orderby) {
+        $this->paramChecked = true;
+        $this->checkValueType($value);
+        $this->checkOrderType($orderby);
+    }
+
+    /**
+     * @param string $value
+     * @return void
+     */
+    private function checkValueType($value) {
+        if(is_null($value)) {
+            $this->valueType = 'null';
+        }
+        if(is_string($value)) {
+            $this->valueType = 'string';
+        }
+        if(is_numeric($value)) {
+            $this->valueType = 'numeric';
+        }
+        if(is_array($value)) {
+            $this->valueType = 'array';
+        }
+    }
+
+    /**
+     * @param string $orderby
+     * @return void
+     */
+    private function checkOrderType($orderby) {
+        switch($orderby) {
+            case 'asc':
+                $this->orderby = 'asc';
+                break;
+            case 'desc':
+                $this->orderby = 'desc';
+                break;
+            default:
+                $this->orderby = 'asc';
+        }
+    }
+
+    /**
+     * @param string $value
+     * @return string
+     */
+    private function sortByString($value) {
+        if($this->orderby == 'asc') {
+            return $value;
+        } else {
+            return strrev($value);
+        }
+    }
+
+    /**
+     * @param numeric $value
+     * @return string
+     */
+    private function sortByNumeric($value) {
+        settype($value, 'string');
+        return $this->sortByString($value);
+    }
+
+    /**
+     * @param array $value
+     * @return array
+     */
+    private function popSortByArray($value) {
+        if(empty($value)) {
+            return 'This array is empty!';
+        }
+        if(sizeof($value) <= 1) {
+            return $value;
+        }
+
+        $len = count($value);
+
+        for($i = 0; $i < $len; $i++) {
+            for($j = $len - 1; $j > $i; $j--) {
+                if($this->orderby == 'asc'
+                    ? $value[$j] < $value[$j - 1]
+                    : $value[$j] > $value[$j - 1]) {
+                    $temp = $value[$j];
+                    $value[$j] = $value[$j - 1];
+                    $value[$j - 1] = $temp;
+                }
             }
+        }
+        return $value;
+    }
 
-            for($i = 1; $i < $_size; $i++) {
-                if($orderby == 'aesc' ? $arr[$i] <= $k : $arr[$i] >= $k) {
-                    $x[] = $arr[$i];
+    /**
+     * @param array $value
+     * @return array
+     */
+    private function quickSortByArray($value) {
+        if(sizeof($value) > 1) {
+            $k = $value[0];
+            $left = array();
+            $right = array();
+            $len = count($value);
+
+            for($i = 1; $i < $len; $i++) {
+                if($this->orderby == 'asc' ? $value[$i] <= $k : $value[$i] >= $k) {
+                    $left[] = $value[$i];
                 } else {
-                    $y[] = $arr[$i];
+                    $right[] = $value[$i];
                 }
             }
 
-            $x = $this->quickSort($x, $orderby);
-            $y = $this->quickSort($y, $orderby);
+            $left = $this->quickSortByArray($left, $this->orderby);
+            $right = $this->quickSortByArray($right, $this->orderby);
 
-            return array_merge($x, array($k), $y);
+            return array_merge($left, array($k), $right);
         } else {
-            return $arr;
+            return $value;
         }
     }
-} 
+
+    /**
+     * @param mixed $value
+     * @param string orderby
+     * @param string sortType
+     * @return array
+     */
+    private function callSort($value, $orderby, $sortType) {
+        if(!$this->paramChecked) {
+            $this->checkParam($value, $orderby);
+        }
+        switch($this->valueType) {
+            case 'null':
+                return null;
+            case 'string':
+                return $this->sortByString($value);
+            case 'numeric':
+                return $this->sortByNumeric($value);
+            case 'array':
+                $sortTypeByArray = $sortType . 'ByArray';
+                return $this->$sortTypeByArray($value);
+            default:
+                return 'Put [Array | String | Number] Value Type Please!';
+        }
+    }
+
+    /**
+     * @param mixed $value
+     * @param string $orderby
+     * @return array
+     */
+    public function popSort($value, $orderby = 'asc') {
+        return $this->callSort($value, $orderby, 'popSort');
+    }
+
+    /**
+     * @param mixed $value
+     * @param string $orderby
+     * @return array
+     */
+    public function quickSort($value, $orderby = 'asc') {
+        return $this->callSort($value, $orderby, 'quickSort');
+    }
+}
